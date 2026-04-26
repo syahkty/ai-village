b134a89c1c4a41473edf67f79bb72668ddb43b51
Syahkty <andisyahktu46@gmail.com>
2026-04-27 00:19:38 +0800

    Auto Deploy Update

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ git push origin main
 
 echo "⚙️ Menghubungkan ke VPS untuk Update & Restart..."
 # Mengeksekusi perintah langsung di dalam VPS melalui SSH
-ssh -i d/Projek/cloud minecraft/ssh-key-2026-02-09.key ubuntu@168.110.207.69 "cd /home/ubuntu/Projek/ai-village && git pull origin main && npm install && pm2 restart pioneer-bot"
+ssh -i "d/Projek/cloud minecraft/ssh-key-2026-02-09.key" ubuntu@168.110.207.69 "cd /home/ubuntu/Projek/ai-village && git pull origin main && npm install && pm2 restart pioneer-bot"
 
 echo "✅ Deploy Selesai! Bot sudah diperbarui."
