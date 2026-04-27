fcf88c7d35e3dbaf25a8b3e8593db5425b951de4
Syahkty <andisyahktu46@gmail.com>
2026-04-27 20:18:54 +0800

    Auto Deploy Update

--- pioneer.js
+++ pioneer.js
@@ -78,6 +78,20 @@ bot.on('chat', async (username, message) => {
     }, 300);
   }
 
+  // === FITUR TES LOMPAT MANUAL ===
+  else if (message === 'lompat') {
+    bot.chat('Hiaaa! (Tes lompat di tempat)');
+    
+    // Tekan tombol spasi
+    bot.setControlState('jump', true);
+    
+    // Tahan spasi selama 300 milidetik (waktu standar Minecraft untuk lompatan penuh)
+    setTimeout(() => {
+      // Lepas tombol spasi
+      bot.setControlState('jump', false);
+    }, 300);
+  }
+
   // === FITUR KONFIRMASI (BARU) ===
   else if (message === 'menerima kayu') {
     bot.pathfinder.setGoal(null) // Menghentikan gerakan bot sepenuhnya
